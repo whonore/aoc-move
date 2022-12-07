e5989fc49085b342f734cd1b76849ae2957b41e6
whonore <wolfhonore@gmail.com>
2022-12-07 17:02:09 -0500

    feat: add day 7 inputs

---
+++ aoc22/inputs/d07.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from move import BcsScript, Bytes, Vector
+
+NAME = Path(__file__).stem
+INPUT = Path(__file__).with_suffix(".in")
+OUTPUT = Path(__file__).with_suffix(".bcs")
+
+
+def parse(data: str) -> bytes:
+    with BcsScript(NAME) as script:
+        cmds = Vector(
+            list(map(Bytes, data.strip().split("\n"))),
+            name="INPUT",
+            type_="vector<u8>",
+        )
+        script.write(cmds)
+        return script.run()
+
+
+if __name__ == "__main__":
+    data = INPUT.read_text(encoding="utf-8")
+    out = parse(data)
+    OUTPUT.write_bytes(out)
